7bab6265b4d1c9012fada27ca835138803d3111b
Kristiyan Borisov <genkoph@protonmail.com>
2022-12-02 11:44:24 +0200

    feat: init turborepo with small modifications

---
+++ apps/web/tests/index.test.tsx
@@ -0,0 +1,15 @@
+import { render } from "@testing-library/react";
+import Home from "../src/pages/index";
+
+describe("Home", () => {
+  it("renders page", () => {
+    // arrange
+    const { getByRole } = render(<Home />);
+
+    // act
+    const heading = getByRole("heading");
+
+    // assert
+    expect(heading).toHaveTextContent("Web");
+  });
+});
