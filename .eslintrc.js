35b066fafa15a67eeba92ec956e284afbf7616e1
Stoyan Merdzhanov <stoyan.merdzhanov@gmail.com>
2022-12-05 13:08:24 +0200

    refactor: rename all local packages to have name prefix @packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
+// Root, i.e. baseline ESLint configuration and all apps or packages would extend it
 module.exports = {
   root: true,
-  // This tells ESLint to load the config from the package `eslint-config-custom`
+  // This tells ESLint to load the config from the package `@packages/eslint-config-custom`
   extends: ["custom"],
   settings: {
     next: {
