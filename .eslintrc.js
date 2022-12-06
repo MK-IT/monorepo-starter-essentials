e47e977649d54e9c02937481cd247ba4a095236a
Kristiyan Borisov <genkoph@protonmail.com>
2022-12-06 12:07:11 +0200

    fix: eslint extending wrong package

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@
 module.exports = {
   root: true,
   // This tells ESLint to load the config from the package `@packages/eslint-config-custom`
-  extends: ["custom"],
+  extends: ["@packages/eslint-config-custom"],
   settings: {
     next: {
       rootDir: ["apps/*/"],
